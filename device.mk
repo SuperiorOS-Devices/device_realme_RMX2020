d28f5150ffe4e73bc1c4cdc5bcfd3e5fdc3069a3
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-11 21:26:18 +0530

    RMX2020: Don't override APNs config file
    Change-Id: If20962fa088558734d17c8e1594106606ef8a422
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -74,10 +74,6 @@ PRODUCT_COPY_FILES += \
 
 TARGET_EXCLUDES_AUDIOFX := true
 
-# APN
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0 \
