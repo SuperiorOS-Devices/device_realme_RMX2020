2f1df8f1ee30d079e6a9c9d0a4c829ebf0a92973
willylakshit <lakshitt428@gmail.com>
2023-04-14 09:23:23 +0530

    RMX2020: SuperiorOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX2020.mk
+    $(LOCAL_DIR)/superior_RMX2020.mk
+
+COMMON_LUNCH_CHOICES := \
+    superior_RMX2020-user \
+    superior_RMX2020-userdebug \
+    superior_RMX2020-eng
--- superior_RMX2020.mk
+++ superior_RMX2020.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_RMX2020
+PRODUCT_NAME := superior_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -26,8 +26,7 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
-# ArrowOS additions
-DEVICE_MAINTAINER := sarthakroy2002
+# SuperiorOS additions
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Build info
